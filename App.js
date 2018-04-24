51889996ccf9b34fbeec6a7ac53211549196e381
Ori Harel <ori.harel@gmail.com>
2018-04-24 14:19:36 +0300

    with text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Svg, {G} from 'react-native-svg';
+import Svg, {G, Text as SVGText} from 'react-native-svg';
 import {
     Animated,
     StyleSheet,
@@ -32,22 +32,37 @@ export default class App extends Component<Props> {
     constructor(props) {
         super(props);
         this.state = {
-            animValue: new Animated.Value(0.1),
+            animPie: new Animated.Value(0.1),
+            animText: new Animated.Value(0),
         };
 
     }
 
     resetPie = ()=>{
-        this.state.animValue.setValue(0.1);
+        this.state.animPie.setValue(0.1);
     };
 
     animate = ()=>{
 
         Animated.timing(
-            this.state.animValue,
+            this.state.animPie,
             {
                 toValue: 2,
-                duration: 2000,
+                duration: 500,
+                easing: Easing.inOut(Easing.quad)
+            }
+        ).start(()=>{
+            setTimeout(this.resetPie, 2000);
+        });
+    };
+
+    animateText = ()=>{
+
+        Animated.timing(
+            this.state.animPie,
+            {
+                toValue: 2,
+                duration: 500,
                 easing: Easing.inOut(Easing.quad)
             }
         ).start(()=>{
@@ -58,32 +73,57 @@ export default class App extends Component<Props> {
     componentDidUpdate(){}
 
     render() {
-        let endAngle = Animated.multiply(this.state.animValue, Math.PI);
+        let endAngle = Animated.multiply(this.state.animPie, Math.PI);
         return (
             <View style={styles.container}>
-                <Svg
-                    width={200}
-                    style={styles.pieSVG}
-                    height={200}
-                    viewBox={`-100 -100 200 200`}
-                >
-                    <G>
-                        {
-                            demoData.map( (item, index) =>{
-                                return (
-                                    <AnimatedShape
-                                        index={index}
-                                        endAngle={endAngle}
-                                        color={item.color}
-                                        data={demoData}
-                                        key={'pie_shape_' + index}
-                                    />
-                                )
-                            })
-                        }
-                    </G>
-                </Svg>
-                <Button onPress={this.animate} title={'Animate'}/>
+                <View style={styles.container}>
+                    <Svg
+                        width={200}
+                        style={styles.pieSVG}
+                        height={200}
+                        viewBox={`-100 -100 200 200`}
+                    >
+                        <G>
+                            {
+                                demoData.map( (item, index) =>{
+                                    return (
+                                        <AnimatedShape
+                                            index={index}
+                                            endAngle={endAngle}
+                                            color={item.color}
+                                            data={demoData}
+                                            key={'pie_shape_' + index}
+                                        />
+                                    )
+                                })
+                            }
+                        </G>
+                    </Svg>
+                    <View style={styles.button}>
+                        <Button onPress={this.animate} title={'Animate'}/>
+                    </View>
+                </View>
+                <View style={styles.container}>
+                    <Svg
+                        height="70"
+                        width="400"
+                    >
+                        <SVGText
+                            fill="none"
+                            stroke="#de1e2a"
+                            fontSize="40"
+                            fontWeight="bold"
+                            x="200"
+                            y="50"
+                            textAnchor="middle"
+                        >Chain React 2018</SVGText>
+                    </Svg>
+                    <View style={styles.button}>
+                        <Button onPress={this.animateText} title={'Animate'}/>
+                    </View>
+                </View>
+
+
             </View>
         );
     }
@@ -106,4 +146,7 @@ const styles = StyleSheet.create({
         shadowRadius: 12.5,
         shadowOpacity: 1,
     },
+    button: {
+        marginTop: 20
+    }
 });
